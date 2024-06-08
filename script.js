bcdcb35f3c6885694d98f015e115db3277c211fb
Mohd Zaid Mirza <143643546+zaidmirza24@users.noreply.github.com>
2024-06-08 06:22:49 +0000

    projects added

--- script.js
+++ script.js
@@ -3,12 +3,32 @@ let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('.nav-ul');
 let toggle = document.querySelector('.toggle');
 let cancel = document.querySelector('.cancel');
-hamburger.addEventListener('click',()=>{
+hamburger.addEventListener('click', () => {
 
     toggle.style.display = 'flex';
 })
-cancel.addEventListener('click',()=>{
+cancel.addEventListener('click', () => {
     toggle.style.display = 'none';
 
 
 })
+// rgb ex code generator:
+
+// console.log(red, blue, green)
+
+let capsule_border = document.querySelectorAll('.card-capsule');
+
+// console.log(capsule_border);
+capsule_border.forEach((element)=>{
+    let red = Math.floor(Math.random() * ((255) + 1))
+    let blue = Math.floor(Math.random() * ((255) + 1))
+    let green = Math.floor(Math.random() * ((255) + 1))
+    let a = Math.random() * ((1))
+    // console.log(element)
+    element.style.borderColor = `rgb(${red},${blue},${green})`;
+})
+
+
+
+
+// capsule_border.style.borderColor  = `rgb(${red},${blue},${green})`
